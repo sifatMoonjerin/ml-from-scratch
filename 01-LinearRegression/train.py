37d800e406a9e89dbc9c78ad8596007f29e4e605
sifatMoonjerin <moonjerin.sifat@gmail.com>
2023-02-28 23:19:13 +0600

    start

---
+++ 01-LinearRegression/train.py
@@ -0,0 +1,22 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+import matplotlib.pyplot as plt
+from LinearRegression import LinearRegression
+
+X, y = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=4)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+fig = plt.figure(figsize=(8,6))
+plt.scatter(X[:, 0], y, color = "b", marker = "o", s = 30)
+plt.show()
+
+reg = LinearRegression(lr=0.01)
+reg.fit(X_train,y_train)
+predictions = reg.predict(X_test)
+
+def mse(y_test, predictions):
+    return np.mean((y_test-predictions)**2)
+
+mse = mse(y_test, predictions)
+print(mse)
